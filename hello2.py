a7194dee24a4c56dddb39e54e965b0e1a7c759b6
maboese <mboese@sed.ethz.ch>
2024-02-16 08:03:58 +0100

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,1 +1,6 @@
+import obspy
+from obspy.clients.fdsn import Client
 print("Hello Binder")
+year = 2023
+cat = Client("IRIS").get_events(minmag=6, maxmag=8, starttime=obspy.UTCDateTime(str(year) + \"-01-01T00:00\"), endtime=obspy.UTCDateTime(str(year+1) + \"-01-01T00:00\"))
+print(cat)
